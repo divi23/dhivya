6179743c852e4788ba49c6a58374cb871f412ec9
divi23 <46857566+divi23@users.noreply.github.com>
2019-08-14 20:48:16 +0530

    Create median.c

---
+++ median.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int i,n;
+	float median,a[n];
+	printf("enter the number of element:");
+	scanf("%d",&n);
+	printf("enter %d of an element");
+	for(i=0;i<n;i++)
+	scanf("%f\n",&a[i]);
+	if(n%2==0)
+	median=(a[(n/2)-1]+a[(n/2)])/2;
+	else
+	median=a[(n/2)];
+	for(i=0;i<n;i++)
+	printf("%f\t",a[i]);
+	printf("median is %f\n",median);
+}
